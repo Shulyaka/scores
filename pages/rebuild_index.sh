f38185a21a4c19d2118ff8914c1c6f0d2d8419ce
Denis Shulyaka <Shulyaka@gmail.com>
2020-08-23 18:37:20 +0300

    Thumbnails

--- pages/rebuild_index.sh
+++ pages/rebuild_index.sh
@@ -9,11 +9,11 @@ do
 	if [ "${FILE##*.}" == "pdf" ]
 	then
 		THUMBNAIL="images/${FILE%.pdf}.png"
-		gs -q -dBATCH -dNOPAUSE -sDEVICE=pngalpha -dMAxBitmap=500000000 -dAlignToPixles=0 -dGridFitTT=0 -dDEVICEHEIGHT=120 -r10.3x10.3 -sOutputFile="$THUMBNAIL" "$FILE" || THUMBNAIL="images/filetype_pdf.png"
+		gs -q -dBATCH -dNOPAUSE -sDEVICE=pngalpha -dMAxBitmap=500000000 -dAlignToPixles=0 -dGridFitTT=0 -g85x120 -r10.3x10.3 -sOutputFile="$THUMBNAIL" "$FILE" || THUMBNAIL="images/filetype_pdf.png"
 	else
 		THUMBNAIL="images/filetype_${FILE##*.}.png"
 	fi
-	echo "<li><a href='$FILE' title='$FILE'><em>$FILE</em><span></span><img src='$THUMBNAIL' height='120' alt='$FILE' /></a></li>" >> index.html
+	echo "<li><a href='$FILE' title='$FILE'><em>$FILE</em><img src='$THUMBNAIL' width='85' height='120' alt='$FILE' /></a></li>" >> index.html
 done
 
 cat footer.html >> index.html
