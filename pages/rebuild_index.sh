f1414ca5da2e6c249f45f3b266bb7427029d40df
Denis Shulyaka <Shulyaka@gmail.com>
2020-08-23 15:34:37 +0300

    Thumbnails

--- pages/rebuild_index.sh
+++ pages/rebuild_index.sh
@@ -6,7 +6,14 @@ for FILE in *.{pdf,mid,midi}
 do
 	test -f "$FILE" || continue
 	echo "$FILE"
-	echo "<li><a href='$FILE' title='$FILE'><em-pdf>$FILE</em-pdf><span></span><img src='images/filetype_${FILE##*.}.png' width='120' height='120' alt='$FILE' /></a></li>" >> index.html
+	if [ "${FILE##*.}" == "pdf" ]
+	then
+		THUMBNAIL="images/${FILE%.pdf}.png"
+		gs -q -dBATCH -dNOPAUSE -sDEVICE=pngalpha -dMAxBitmap=500000000 -dAlignToPixles=0 -dGridFitTT=0 -dDEVICEHEIGHT=120 -r10.3x10.3 -sOutputFile="$THUMBNAIL" "$FILE" || THUMBNAIL="images/filetype_pdf.png"
+	else
+		THUMBNAIL="images/filetype_${FILE##*.}.png"
+	fi
+	echo "<li><a href='$FILE' title='$FILE'><em-pdf>$FILE</em-pdf><span></span><img src='$THUMBNAIL' width='120' height='120' alt='$FILE' /></a></li>" >> index.html
 done
 
 cat footer.html >> index.html
