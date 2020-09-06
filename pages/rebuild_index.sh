e641565a49b00f4c2d5b2ee61f43a910e523ad91
Denis Shulyaka <Shulyaka@gmail.com>
2020-09-06 16:05:37 +0300

    configurable name

--- pages/rebuild_index.sh
+++ pages/rebuild_index.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sed header.html -e "s/\$AUTHOR/$(git shortlog -s -n|head -n 1|sed -e 's/\s*[0-9]*\s*//')/g" > index.html
+NAME=Scores
+sed header.html -e "s/\$AUTHOR/$(git shortlog -s -n|head -n 1|sed -e 's/\s*[0-9]*\s*//')/g" -e "s/\$NAME/$NAME/g" > index.html
 
 for FILE in *.{pdf,mid,midi}
 do
@@ -18,5 +19,5 @@ done
 
 sed footer.html -e "s/\$LILYPOND_VERSION/$(lilypond --version|head -n 1)/g" -e "s/\$REVISION/$(git rev-parse --short HEAD)/g" -e "s|\$REMOTE|$(git remote get-url origin)|g" >> index.html
 
-rm -f scores.zip
-zip -T scores.zip *.{pdf,mid,midi}
+rm -f "$NAME.zip"
+zip -T "$NAME.zip" *.{pdf,mid,midi}
